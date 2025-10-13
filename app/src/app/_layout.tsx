7a0b1b023849fcc14fe8da68fce0b7c5d295fa40
tnychn <tnychn@protonmail.com>
2025-10-13 17:48:49 +0800

    feat(app): update user interface and API calling
    Also add basic text-to-speech via expo-speech module.

--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -1,9 +1,24 @@
+import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 
 const client = new QueryClient();
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Feather-Bold": require("@/fonts/Feather-Bold.otf"),
+  });
+
+  React.useEffect(() => {
+    if (loaded) SplashScreen.hide();
+  });
+
+  if (!loaded) return null;
+
   return (
     <QueryClientProvider client={client}>
       <Stack screenOptions={{ headerShown: false }} />
--- app/src/app/index.tsx
+++ app/src/app/index.tsx
@@ -1,15 +1,41 @@
-import React, { useEffect } from "react";
-import { Alert, Pressable, Text, View } from "react-native";
-import { useMutation } from "@tanstack/react-query";
+import React from "react";
+import { ActivityIndicator, Alert, Pressable, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import {
   AudioModule,
   AudioQuality,
+  RecordingOptions,
   setAudioModeAsync,
   useAudioRecorder,
   useAudioRecorderState,
 } from "expo-audio";
+import * as Speech from "expo-speech";
 import axios from "axios";
+import { AudioLinesIcon, MicIcon, Repeat2Icon, Volume2Icon } from "lucide-react-native";
 import tw from "twrnc";
+
+const RECORDING_OPTIONS: RecordingOptions = {
+  extension: ".wav",
+  bitRate: 128_000,
+  sampleRate: 48_000,
+  numberOfChannels: 1,
+  ios: {
+    extension: ".wav",
+    outputFormat: "lpcm",
+    audioQuality: AudioQuality.HIGH,
+  },
+  android: {
+    outputFormat: "aac_adts",
+    audioEncoder: "aac",
+  },
+};
+
+type Transcript = {
+  id: string;
+  text: string;
+  phonemes: string[];
+};
 
 type Result = {
   feedback: string;
@@ -26,29 +52,26 @@ type Result = {
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export default function HomeScreen() {
-  const recorder = useAudioRecorder({
-    extension: ".wav",
-    bitRate: 128_000,
-    sampleRate: 48_000,
-    numberOfChannels: 1,
-    ios: {
-      extension: ".wav",
-      outputFormat: "lpcm",
-      audioQuality: AudioQuality.HIGH,
-    },
-    android: {
-      outputFormat: "aac_adts",
-      audioEncoder: "aac",
+  const recorder = useAudioRecorder(RECORDING_OPTIONS);
+  const recorderState = useAudioRecorderState(recorder);
+
+  const query = useQuery({
+    queryKey: ["transcript"],
+    queryFn: async () => {
+      const response = await axios.get(`${API_URL}/`);
+      return response.data as Transcript;
     },
   });
-  const recorderState = useAudioRecorderState(recorder);
 
   const mutation = useMutation({
     mutationFn: async (uri: string) => {
+      if (!query.data) {
+        throw new Error("No transcript loaded.");
+      }
       const data = new FormData();
       // @ts-expect-error React Native FormData issue
       data.append("audio", { uri, name: "audio.wav", type: "audio/vnd.wav" });
-      const response = await axios.post(`${API_URL}/teach/1`, data, {
+      const response = await axios.post(`${API_URL}/${query.data.id}/teach`, data, {
         validateStatus: () => true, // do not throw as error on non-ok status
       });
       if (response.status !== 200) {
@@ -58,7 +81,7 @@ export default function HomeScreen() {
     },
   });
 
-  useEffect(() => {
+  React.useEffect(() => {
     (async () => {
       const status = await AudioModule.requestRecordingPermissionsAsync();
       if (!status.granted) return Alert.alert("Permission Denied");
@@ -83,18 +106,76 @@ export default function HomeScreen() {
   }
 
   return (
-    <View style={tw`flex-1 items-center justify-center gap-2`}>
-      <Pressable
-        style={({ pressed }) =>
-          tw.style("rounded-lg px-5 py-3", pressed && "opacity-50", {
-            backgroundColor: recorderState.isRecording ? "red" : "cyan",
-          })
-        }
-        onPress={() => (recorderState.isRecording ? stopRecording() : startRecording())}>
-        {recorderState.isRecording ? <Text>Stop</Text> : <Text>Start</Text>}
-      </Pressable>
-      {mutation.isSuccess && <Text>{mutation.data?.feedback ?? "SILENCE"}</Text>}
-      {mutation.isError && <Text style={tw`text-red-500`}>{mutation.error.message}</Text>}
-    </View>
+    <SafeAreaView style={tw`flex-1 items-center justify-between gap-2 px-4`}>
+      <View style={tw`w-full flex-row items-center justify-center p-2`}>
+        <Text style={[tw`text-2xl text-green-500`, { fontFamily: "Feather-Bold" }]}>yaplingo</Text>
+        {/* <Text style={tw`text-lg font-medium`}>{new Date().toLocaleDateString()}</Text> */}
+      </View>
+      <View style={tw`gap-4`}>
+        {query.isFetching ? (
+          <ActivityIndicator size="large" />
+        ) : (
+          <>
+            <View style={tw`items-center justify-center gap-4 rounded-xl bg-white p-8 `}>
+              {query.isSuccess && (
+                <>
+                  <Text selectable style={tw`text-center text-3xl font-medium`}>
+                    {query.data.text}
+                  </Text>
+                  <Text style={tw`text-center text-2xl font-medium`}>
+                    {query.data.phonemes.join(" ")}
+                  </Text>
+                </>
+              )}
+            </View>
+          </>
+        )}
+      </View>
+      <View style={tw`w-full flex-row items-center justify-between p-8`}>
+        {!query.isFetching && (
+          <>
+            {!recorderState.isRecording && (
+              <Pressable
+                style={({ pressed }) =>
+                  tw.style("rounded-full bg-violet-500 p-4", pressed && "opacity-80")
+                }
+                onPress={async () => {
+                  if (!(await Speech.isSpeakingAsync()))
+                    Speech.speak(query.data?.text ?? "", {
+                      rate: 0.75,
+                      // language: "de-DE",
+                    });
+                }}>
+                <Volume2Icon color="white" size={24} />
+              </Pressable>
+            )}
+            <Pressable
+              style={({ pressed }) =>
+                tw.style(
+                  "mx-auto rounded-full p-6",
+                  pressed && "opacity-80",
+                  recorderState.isRecording ? "bg-red-500" : "bg-blue-500",
+                )
+              }
+              onPress={() => (recorderState.isRecording ? stopRecording() : startRecording())}>
+              {recorderState.isRecording ? (
+                <AudioLinesIcon color="white" size={32} />
+              ) : (
+                <MicIcon color="white" size={32} />
+              )}
+            </Pressable>
+            {!recorderState.isRecording && (
+              <Pressable
+                style={({ pressed }) =>
+                  tw.style("rounded-full bg-gray-500 p-4", pressed && "opacity-80")
+                }
+                onPress={() => query.refetch()}>
+                <Repeat2Icon color="white" size={24} />
+              </Pressable>
+            )}
+          </>
+        )}
+      </View>
+    </SafeAreaView>
   );
 }
